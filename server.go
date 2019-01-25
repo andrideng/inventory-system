768982e3e342f4f1185fda034000f20b3c8348fe
Andri Deng <andri.deng@bvolo.com>
2019-01-25 22:13:00 +0700

    first commit

---
+++ server.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-ozzo/ozzo-dbx"
+	"github.com/go-ozzo/ozzo-routing/content"
+	"github.com/go-ozzo/ozzo-routing/cors"
+
+	"github.com/andrideng/inventory-system/app"
+	"github.com/andrideng/inventory-system/errors"
+	routing "github.com/go-ozzo/ozzo-routing"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/Sirupsen/logrus"
+)
+
+func main() {
+	// - load application configurations
+	if err := app.LoadConfig("./config"); err != nil {
+		panic(fmt.Errorf("Invalid application configuration: %s", err))
+	}
+
+	// - load error messages
+	if err := errors.LoadMessages(app.Config.ErrorFile); err != nil {
+		panic(fmt.Errorf("Failed to read the error message file: %s", err))
+	}
+
+	// - create the logger
+	logger := logrus.New()
+
+	// - connect to the database
+	db, err := dbx.MustOpen(app.Config.Dialect, app.Config.DSN)
+	if err != nil {
+		panic(err)
+	}
+	db.LogFunc = logger.Infof
+
+	// - wire up API routing
+	http.Handle("/", buildRouter(logger, db))
+
+	// - start the server
+	address := fmt.Sprintf(":%v", app.Config.ServerPort)
+	logger.Infof("server %v is started at %v\n", app.Version, address)
+	panic(http.ListenAndServe(address, nil))
+}
+
+func buildRouter(logger *logrus.Logger, db *dbx.DB) *routing.Router {
+	router := routing.New()
+
+	router.Use(
+		app.Init(logger),
+		content.TypeNegotiator(content.JSON),
+		cors.Handler(cors.Options{
+			AllowOrigins: "*",
+			AllowHeaders: "*",
+			AllowMethods: "*",
+		}),
+		app.Transactional(db),
+	)
+
+	rg := router.Group("/api")
+
+	// - set basic route
+	rg.Get("/", func(c *routing.Context) error {
+		return c.Write("Welcome To Inventory System API " + app.Version)
+	})
+	// - set health check route
+	rg.Get("/ping", func(c *routing.Context) error {
+		return c.Write("PONG!")
+	})
+
+	return router
+}
